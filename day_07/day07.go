c7a20d07e503ae63cbc964cef762534fc9776849
Mike Digdon <32070772+digdon@users.noreply.github.com>
2024-12-08 17:02:40 -0400

    Moved the original solution to an 'old folder and re-did this using trees. Soooo much faster now.

--- day_07/day07.go
+++ day_07/day07.go
@@ -24,19 +24,13 @@ func main() {
 	}
 
 	begin := time.Now()
-	part1Results := part1(inputLines)
-	part1Time := time.Since(begin)
-
+	fmt.Println("Part 1:", evaluateEquations(inputLines, false), time.Since(begin))
 	begin = time.Now()
-	part2Results := part2(inputLines)
-	part2Time := time.Since(begin)
-
-	fmt.Printf("Part 1: %d (in %s)\n", part1Results, part1Time)
-	fmt.Printf("Part 2: %d (in %s)\n", part2Results, part2Time)
+	fmt.Println("Part 2:", evaluateEquations(inputLines, true), time.Since(begin))
 }
 
-func part1(inputLines []string) int {
-	var part1Total int
+func evaluateEquations(inputLines []string, includeConcat bool) int {
+	var runningTotal int
 
 	for _, line := range inputLines {
 		colonIdx := strings.Index(line, ": ")
@@ -49,105 +43,77 @@ func part1(inputLines []string) int {
 			values = append(values, value)
 		}
 
-		for addMul := 0; addMul < (1 << (len(values) - 1)); addMul++ {
-			result := values[0]
+		root := Node{value: values[0], operation: ADD, result: values[0], parent: nil, children: nil}
 
-			for i := 1; i < len(values); i++ {
-				if addMul&(1<<(i-1)) != 0 {
-					result += values[i]
-				} else {
-					result *= values[i]
-				}
-			}
-
-			if result == targetValue {
-				part1Total += targetValue
-				break
-			}
+		if process(targetValue, root, values[1:], includeConcat) {
+			runningTotal += targetValue
 		}
 	}
 
-	return part1Total
+	return runningTotal
 }
 
-func part2(inputLines []string) int {
-	var part2Total int
+func process(targetValue int, node Node, values []int, includeConcat bool) bool {
+	if node.result == targetValue {
+		// displayResult(&node, targetValue)
+		return true
+	} else if node.result > targetValue {
+		return false
+	}
 
-	for _, line := range inputLines {
-		colonIdx := strings.Index(line, ": ")
-		targetValue, _ := strconv.Atoi(line[:colonIdx])
-		parts := strings.Split(line[colonIdx+2:], " ")
-		values := []int{}
+	if len(values) == 0 {
+		return false
+	}
 
-		for _, part := range parts {
-			value, _ := strconv.Atoi(part)
-			values = append(values, value)
-		}
+	node.children = append(node.children, Node{value: values[0], operation: ADD, result: node.result + values[0], parent: &node, children: nil})
+	node.children = append(node.children, Node{value: values[0], operation: MUL, result: node.result * values[0], parent: &node, children: nil})
 
-		cache := map[string]bool{}
+	if includeConcat {
+		concatValue, _ := strconv.Atoi(strconv.Itoa(node.result) + strconv.Itoa(values[0]))
+		node.children = append(node.children, Node{value: values[0], operation: CONCAT, result: concatValue, parent: &node, children: nil})
+	}
 
-		for matched, concat := false, 0; !matched && concat < (1<<(len(parts)-1)); concat++ {
-			for addMul := 0; addMul < (1 << (len(values) - 1)); addMul++ {
-				var sb strings.Builder
-				for i := 0; i < len(values)-1; i++ {
-					if concat&(1<<i) != 0 {
-						sb.WriteString("|")
-					} else if addMul&(1<<i) != 0 {
-						sb.WriteString("+")
-					} else {
-						sb.WriteString("*")
-					}
-				}
-
-				key := sb.String()
-				if cache[key] {
-					continue
-				}
-
-				result := values[0]
-
-				for i := 1; i < len(values); i++ {
-					if concat&(1<<(i-1)) != 0 {
-						result, _ = strconv.Atoi(strconv.Itoa(result) + strconv.Itoa(values[i]))
-					} else if addMul&(1<<(i-1)) != 0 {
-						result += values[i]
-					} else {
-						result *= values[i]
-					}
-				}
-
-				if result == targetValue {
-					display(targetValue, values, addMul, concat, result)
-					part2Total += targetValue
-					matched = true
-					break
-				}
-
-				cache[key] = true
-			}
+	for _, child := range node.children {
+		if process(targetValue, child, values[1:], includeConcat) {
+			return true
 		}
 	}
 
-	return part2Total
+	return false
 }
 
-func display(targetValue int, values []int, addMul, concat, result int) {
-	fmt.Print("Target: ", targetValue, " -> ")
+func displayResult(node *Node, result int) {
+	sb := ""
+	for node != nil {
+		sb = strconv.Itoa(node.value) + sb
 
-	for i, value := range values {
-		if i > 0 {
-			if concat&(1<<(i-1)) != 0 {
-				print(" || ")
-			} else if addMul&(1<<(i-1)) != 0 {
-				print(" + ")
-			} else {
-				print(" * ")
+		if node.parent != nil {
+			switch node.operation {
+			case ADD:
+				sb = " + " + sb
+			case MUL:
+				sb = " * " + sb
+			case CONCAT:
+				sb = " || " + sb
 			}
 		}
 
-		print(value)
+		node = node.parent
 	}
 
-	fmt.Println(" = ", result)
-
+	fmt.Println(result, ": ", sb)
 }
+
+type Node struct {
+	value     int
+	operation int
+	result    int
+	parent    *Node
+	children  []Node
+}
+
+const (
+	ADD = iota
+	MUL
+	CONCAT
+)
