80aaaf67c8a9adb0905d423b982ee97da53fb1cd
Mike Digdon <32070772+digdon@users.noreply.github.com>
2024-12-10 14:12:15 -0400

    Refactor - now both parts share the same code

--- day_09/day09.go
+++ day_09/day09.go
@@ -28,9 +28,9 @@ func main() {
 	// input := "12345"
 
 	begin := time.Now()
-	fmt.Println("Part 1:", partOne(input), time.Since(begin))
+	fmt.Println("Part 1:", defrag(input, false), time.Since(begin))
 	begin = time.Now()
-	fmt.Println("Part 2:", partTwo(input), time.Since(begin))
+	fmt.Println("Part 2:", defrag(input, true), time.Since(begin))
 }
 
 func generateBlocks(input string) (*Block, *Block) {
@@ -67,68 +67,13 @@ func generateBlocks(input string) (*Block, *Block) {
 	return head, tail
 }
 
-func partOne(input string) int {
-	head, tail := generateBlocks(input)
-	full, empty := tail, head
-
-	for full != nil && empty != nil {
-		// Find the next non-empty block, from the end
-		for full != nil && full.id == -1 {
-			full = full.prev
-		}
-
-		// Find the next empty block, with capacity, from the beginning
-		for empty != nil && (empty.id != -1 || empty.size == 0) {
-			empty = empty.next
-		}
-
-		if full == nil || empty == nil || full.start < empty.start {
-			break
-		}
-
-		// Calculate the number of items in the non-empty block that can be moved
-		moveCount := min(full.size, empty.size)
-
-		// Process the empty block
-		if moveCount == empty.size {
-			// We're completely filling the empty block, so just update the id
-			empty.id = full.id
-			empty = empty.next
-		} else {
-			// We're only partially filling the empty block, so we need to split it
-			newBlock := &Block{id: full.id, size: moveCount, start: empty.start}
-			empty.prev.next = newBlock
-			newBlock.prev = empty.prev
-			newBlock.next = empty
-			empty.prev = newBlock
-
-			empty.start += moveCount
-			empty.size -= moveCount
-		}
-
-		// Process the non-empty block
-		if moveCount == full.size {
-			// We've moved all of the items from the non-empty block, so we can drop it
-			full = full.prev
-			full.next = nil
-			tail = full
-		} else {
-			// We only moved some items, so change the current size
-			full.size -= moveCount
-		}
-	}
-
-	return checksum(head)
-}
-
-func partTwo(input string) int {
+func defrag(input string, moveFull bool) int {
 	head, tail := generateBlocks(input)
 	full, empty := tail, head
 
 	for {
 		// Find the next non-empty block that hasn't been moved
-		for full != nil && (full.id == -1 || full.moved) {
-			full = full.prev
+		for ; full != nil && (full.id == -1 || full.moved); full = full.prev {
 		}
 
 		if full == nil {
@@ -136,15 +81,28 @@ func partTwo(input string) int {
 			break
 		}
 
-		// Try to find an empty block that can hold the current non-empty block, starting from the head
-		for empty = head; empty != nil && (empty.id != -1 || empty.size < full.size); empty = empty.next {
+		if moveFull {
+			// Try to find an empty block that can hold the entirety of the current non-empty block, starting from the head
+			for empty = head; empty != nil && (empty.id != -1 || empty.size < full.size); empty = empty.next {
+			}
+		} else {
+			// Look for the next empty block that has any non-zero capacity, from the previous empty block
+			for ; empty != nil && (empty.id != -1 || empty.size == 0); empty = empty.next {
+			}
 		}
 
 		if empty == nil || full.start < empty.start {
-			// No empty block big enough for this file - go on to the next non-empty block
-			full = full.prev
-			continue
+			if moveFull {
+				// No empty block big enough for this file - go on to the next non-empty block
+				full = full.prev
+				continue
+			} else {
+				// Can't move non-empty block contents further into the list
+				break
+			}
 		}
+
+		moveCount := min(full.size, empty.size)
 
 		// Found a spot to move the non-empty block to
 		if empty.size == full.size {
@@ -154,16 +112,24 @@ func partTwo(input string) int {
 			full = full.prev
 		} else {
 			// We're only partially filling the empty block, so we need to split it
-			newBlock := &Block{id: full.id, size: full.size, start: empty.start, moved: true}
+			newBlock := &Block{id: full.id, size: moveCount, start: empty.start, moved: true}
 			empty.prev.next = newBlock
 			newBlock.prev = empty.prev
 			newBlock.next = empty
 			empty.prev = newBlock
 
 			// Update the remaining empty block and mark the non-empty block as now empty
-			empty.start += full.size
-			empty.size -= full.size
-			full.id = -1
+			empty.start += moveCount
+			empty.size -= moveCount
+
+			// Adjust the non-empty block, as necessary
+			if !moveFull {
+				full.size -= moveCount
+			}
+
+			if moveFull || full.size == 0 {
+				full.id = -1
+			}
 		}
 	}
 
