cae0d02269b3cd75100d1c22d516d81d0bbc4875
Mike Digdon <32070772+digdon@users.noreply.github.com>
2025-01-02 13:55:59 -0400

    Solution for day 23 part 2

--- day_23/day23.go
+++ day_23/day23.go
@@ -48,11 +48,26 @@ func main() {
 		cmap[one] = true
 	}
 
+	// Part 1
+	fmt.Printf("Part 1: %d (%s)\n", part1(connectionsMap), time.Since(begin))
+
+	// Part 2
+	begin = time.Now()
+	fmt.Printf("Part 2: %s (%s)\n", strings.Join(part2(connectionsMap), ","), time.Since(begin))
+}
+
+func part1(connectionsMap map[string]map[string]bool) int {
 	// For each node, look at each neighbour and try to find a third node (based on the second)
 	// that connects to the first. If a triangle is found, build a set
 	triangles := map[string][]string{}
 
 	for one, cmap := range connectionsMap {
+		// We're only interested in triangles where at least one node starts with 't'.
+		// Adding this check reduces execution time by about 20x
+		if one[0] != 't' {
+			continue
+		}
+
 		for two := range cmap {
 			for three := range connectionsMap[two] {
 				if connectionsMap[three][one] {
@@ -77,5 +92,71 @@ func main() {
 		}
 	}
 
-	fmt.Printf("Part 1: %d (%s)\n", tCount, time.Since(begin))
+	return tCount
+}
+
+func part2(connectionsMap map[string]map[string]bool) []string {
+	// Prep B-K maps
+	r := map[string]bool{}
+	p := map[string]bool{}
+	x := map[string]bool{}
+
+	for node := range connectionsMap {
+		p[node] = true
+	}
+
+	var cliques [][]string
+	bronKerbosch(connectionsMap, r, p, x, &cliques)
+
+	var largestClique []string
+	for _, clique := range cliques {
+		if len(clique) > len(largestClique) {
+			largestClique = clique
+		}
+	}
+
+	sort.Strings(largestClique)
+
+	return largestClique
+}
+
+func bronKerbosch(connectionsMap map[string]map[string]bool, r, p, x map[string]bool, cliques *[][]string) {
+	if len(p) == 0 && len(x) == 0 {
+		clique := make([]string, 0, len(r))
+		for node := range r {
+			clique = append(clique, node)
+		}
+		*cliques = append(*cliques, clique)
+		return
+	}
+
+	for v := range p {
+		newR := copySet(r)
+		newR[v] = true
+		newP := intersect(p, connectionsMap[v])
+		newX := intersect(x, connectionsMap[v])
+
+		bronKerbosch(connectionsMap, newR, newP, newX, cliques)
+
+		delete(p, v)
+		x[v] = true
+	}
+}
+
+func copySet(s map[string]bool) map[string]bool {
+	c := make(map[string]bool, len(s))
+	for k, v := range s {
+		c[k] = v
+	}
+	return c
+}
+
+func intersect(a, b map[string]bool) map[string]bool {
+	c := map[string]bool{}
+	for k := range a {
+		if b[k] {
+			c[k] = true
+		}
+	}
+	return c
 }
